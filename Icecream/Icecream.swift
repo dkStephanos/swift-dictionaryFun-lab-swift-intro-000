774bfb80607da5506d68d16793735ff5ab00a5c0
stephanos <stephanos@goldmail.etsu.edu>
2017-10-03 11:58:20 -0700

    Tests Broken

--- Icecream/Icecream.swift
+++ Icecream/Icecream.swift
@@ -9,20 +9,34 @@
 class Icecream {
     
     // 1. Create the Dictionary here. The name of the variable should be favoriteFlavorsOfIceCream
-    
+    var favoriteFlavorsOfIceCream = ["Joe" : "Peanut Butter and Chocolate", "Tim" : "Natural Vanilla", "Sophie" : "Mexican Chocolate", "Deniz" : "Natural Vanilla", "Tom" : "Mexican Chocolate", "Jim" : "Natural Vanilla", "Susan" : "Cookies 'N' Cream"];
     
     
     
     
     // 2.
-    
+    func names(forFlavor flavor: String) -> [String] {
+        var studentNames = [String]();
+        
+        for (name, favorite) in favoriteFlavorsOfIceCream {
+            if(flavor == favorite) {
+                studentNames.append(name);
+            }
+        }
+        
+        return studentNames;
+    }
     
     
     
     
     
     // 3.
-    
+    func count(forFlavor flavor: String) -> Int {
+        let studentNames = names(forFlavor: flavor);
+        
+        return studentNames.count;
+    }
     
     
     
